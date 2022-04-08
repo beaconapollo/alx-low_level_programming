f8a15dec02d3fd3e5756bb26ad22b1d98c3dc801
mercy83 <down4trust@gmail.com>
2022-04-08 11:13:21 -0700

    Check a number

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,8 +2,9 @@
 #include <time.h>
 #include <stdio.h>
 
-/*Check a number is positive, zero or negative
- * Return (0)*/
+/*Check a number is positive, zero or negative*/
+
+/*Main*/
 int main(void)
 {
 	int n;
