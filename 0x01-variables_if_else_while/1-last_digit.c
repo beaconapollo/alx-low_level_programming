fdf86d04b2a4f8b150dc649d972ff3fd42700105
mercy83 <down4trust@gmail.com>
2022-04-08 12:41:15 -0700

    The Last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,7 +1,7 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/**Description: Compare Last Digit with 5*/
+/**1-last_digit.c: Compare Last Digit with 5*/
 /**
  * main: is Main function
  * Return: 0 if success
