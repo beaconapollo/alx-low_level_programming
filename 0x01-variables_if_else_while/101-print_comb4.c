e91adf7197269914e0ca83424168e9a88681a793
mercy83 <down4trust@gmail.com>
2022-04-10 04:13:09 -0700

    Print combinations of 3 digits

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,45 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success/correct)
+ *
+ */
+int main(void)
+{
+	int ch2
+	int ch1;
+	int ch0;
+
+	for (ch2 = '0'; ch2 <= '9'; ch2++)
+	{
+		for (ch1 = '0'; ch1 <= '9'; ch1++)
+		{
+			for (ch0 = '0'; ch0 <= '9'; ch0++)
+			{
+
+				if (ch2 != ch1 && ch2 != ch0 && ch1 != ch0)
+				{
+					if(ch2 < ch1 && ch2 < ch1  && ch1 < ch0)
+					{
+						putchar(ch2);
+						putchar(ch1);
+						putchar(ch0);
+						if (ch2 == '7' && ch1 == '8' && ch0 == '9')
+						{
+						}
+						else
+						{
+							putchar(',');
+							putchar(' ');
+						}
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
