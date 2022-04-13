95d2afce2ac61d657df092f0f5adf43174a882e6
mercy83 <down4trust@gmail.com>
2022-04-13 11:18:32 -0700

    adds two integers

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,7 +1,8 @@
 #include "main.h"
 /**
  * add - adds two integers
- * @m, n: numbers to test
+ * @m: the first integer to be added
+ * @n: the second integer to be added
  * Return: result of addition of two integers
  *
  */
