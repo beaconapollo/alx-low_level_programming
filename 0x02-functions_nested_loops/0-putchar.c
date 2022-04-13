237b32e2f09ae5494622ee19adbcfc58c611930b
mercy83 <down4trust@gmail.com>
2022-04-12 23:00:20 -0700

    Print string

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,8 +7,18 @@
  * Return: Always 0 (Success/correct)
  *
  */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
 int main(void)
 {
-	printf("_putchar\n");
+	char c[] = "_putchar";
+
+	for (int i = 0; i < 8; i++)
+	{
+		_putchar(c[i]);
+	}	
+	_putchar('\n');
 	return (0);
 }
