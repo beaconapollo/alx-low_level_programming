a413225a1803ae53b0d49610b5d5a9aeb64598ef
mercy83 <down4trust@gmail.com>
2022-04-13 10:56:37 -0700

    Print the 9 times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -22,6 +22,7 @@ void times_table(void)
 					_putchar(',');
 					_putchar(' ');
 					_putchar(' ');
+					_putchar(' ');
 				}
 			}
 			else
@@ -35,7 +36,6 @@ void times_table(void)
 				}
 			}
 		}
-		_putchar('$');
 		_putchar('\n');
 	}
 }
