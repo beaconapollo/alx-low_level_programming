2d8dfb5326ec3ca055a55a6fdf09d79a52f4c7cc
mercy83 <down4trust@gmail.com>
2022-04-13 01:43:12 -0700

    Check whether lowercase

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints the english alphabet a-z 10 times
+ *
+ * Return: Nothing.
+ *
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}	
+	return (0);
+}
