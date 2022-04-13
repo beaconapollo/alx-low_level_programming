5710bacf89ffe4fcd4d694c44715f853be377401
mercy83 <down4trust@gmail.com>
2022-04-13 02:21:04 -0700

    Check +/0-

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * _isalpha - tests whether a character is letter
+ * @c: character to test
+ * Return: 1 if it is a letter otherwise 0
+ *
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0')
+		return (0);
+	}
+}
