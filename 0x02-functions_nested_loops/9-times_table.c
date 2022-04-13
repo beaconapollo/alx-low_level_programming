fa07429ec5c3677939067783a60107ba0ce7b1d0
mercy83 <down4trust@gmail.com>
2022-04-13 07:09:46 -0700

    Commit time tables

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -17,18 +17,22 @@ void times_table(void)
 			if (r <= 9)
 			{
 				_putchar(r + '0');
+				if (m != 9)
+				{
+					_putchar(',');
+					_putchar('  ');
+				}
 			}
 			else
 			{
 				_putchar((r / 10) + '0');
 				_putchar((r % 10) + '0');
+				if (m != 9)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
 			}
-			if(m != 9)
-			{
-				_putchar(',');
-				_putchar(' ');
-			}
-		}
 		_putchar('$');
 		_putchar('\n');
 	}
