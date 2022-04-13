e6f154dd8d7197b59c635a3e093e20f60320fe00
mercy83 <down4trust@gmail.com>
2022-04-13 00:08:54 -0700

    Print alphabet

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,13 +1,1 @@
-#include <unistd.h>
-#include <stdio.h>
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-void _putchar(char c)
-{
-	putchar(c);
-}
+../main.h
