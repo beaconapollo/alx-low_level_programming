76b7167b0547cc6954d6da6acce0c6785232e6e2
mercy83 <down4trust@gmail.com>
2022-04-13 06:39:54 -0700

    Time counter

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -7,24 +7,18 @@
  */
 void jack_bauer()
 {
-	char h1, h0, m1, m0;
+	int hh, mm;
 
-	for (h1 = '0'; h1 <= '9'; h1++)
+	for (hh = 0; hh <= 23; hh++)
 	{
-		for (h0 = '0'; h0 <= '9'; h0++)
+		for (mm = 0; mm <= 59; mm++)
 		{
-			for (m1 = '0'; m1 <= '5'; m1++)
-			{
-				for (m0 = '0'; m0 <= '9'; m0++)
-				{
-					_putchar(h1);
-					_putchar(h0);
-					_putchar(':');
-					_putchar(m1);
-					_putchar(m0);
-					_putchar('\n');
-				}
-			}
+			_putchar((hh / 10) + '0');
+			_putchar((hh % 10) + '0');
+			_putchar(':');
+			_putchar((mm / 10) + '0');
+			_putchar((mm % 10) + '0');
+			_putchar('\n');
 		}
 	}
 }
