51dca2fd8a1310285b43a12859d6f60dde8c830b
mercy83 <down4trust@gmail.com>
2022-04-14 01:45:37 -0700

    Print n times table

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,23 @@
+#include "main.h:"
+/**
+ * print_times_table - prints the less than 15 times table
+ * @n, m: number to test
+ * Return: Nothing
+ *
+ */
+void print_times_table(int n)
+{
+	int i, j, r;
+
+	if (n <= 15 && n >=0)
+	{
+		for (i = 0; i <= n; i++)
+		{
+			for (j = 0; j <= n; j++)
+			{
+				r = i * j;
+				printf("%d, ", r);
+			}
+		}
+	}
+}
