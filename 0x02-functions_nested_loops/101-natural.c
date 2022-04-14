22f2a888fb7947187ed35dd66b8fe52564b0988e
mercy83 <down4trust@gmail.com>
2022-04-14 03:10:49 -0700

    Print sum of num < 1024

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,20 @@
+#include <stdio.h> 
+/**
+ * natural - computes the absolute value of an integer
+ * @n: number to test
+ * Return: absolute value of n
+ *
+ */
+int natural()
+{
+	int sum = 0, i;
+
+	for (i = 1; i < 1024; i++)
+	{
+		if(i % 3 == 0 || i % 5 == 0)
+		{
+			sum += i;
+		}
+	}
+	printf("%d", sum);
+}
