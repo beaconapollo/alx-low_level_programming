3d693d61caba61320572ed79dcd905e187a940dc
beaconapollo <down4trust@gmail.com>
2022-04-15 22:50:29 +0300

    print 0-9

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,18 +2,18 @@
 
 /**
  * print_numbers - prints numbers from 0 to 9
- * @c: test character
+ * Description: Prints the numbers with _putchar
  *
  * Return:
  */
 void print_numbers(void)
 {
-int c;
+	int c;
 
-for (c = '0';c <= '9'; c++)
-{
-_putchar(c);
-}
-_putchar('\n');
+	for (c = '0';c <= '9'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
 }
 
