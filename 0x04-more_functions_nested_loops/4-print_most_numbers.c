a28ba736c3f1cf0290334bb4ccb9052d7b315dcd
beaconapollo <down4trust@gmail.com>
2022-04-15 23:02:15 +0300

    print ex 2&4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
- * print_numbers - entry point
+ * print_most_numbers - entry point
  *
  * Description: Prints the numbers with _putchar
  *
  * Return: void
  */
 
-void print_numbers(void)
+void print_most_numbers(void)
 {
 	int c;
 
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -3,3 +3,4 @@ int _isdigit(int);
 int mul(int, int);
 void print_numbers(void);
 void _putchar(int);
+void print_most_numbers(void);
