963c3aca2dc4a1e28708699730bcffa30ad04742
mercy83 <down4trust@gmail.com>
2022-04-18 04:25:40 -0700

    Prints length of a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strlen - entry point
+ *
+ * Description: returns the length of a string
+ * @s: parameter that points to address
+ * Return: length of a string
+ */
+
+int _strlen(char *s)
+{
+	int len = 0;
+	char *p;
+	p = s;
+	
+	while (*p != '\0')
+	{
+		len++;
+		p++;
+	}
+}
