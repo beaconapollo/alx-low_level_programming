98efea556eb4fd379a382ef9b89e0dc7b1e20cad
beaconapollo <down4trust@gmail.com>
2022-04-19 21:27:36 +0300

    Print character

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * puts2 - entry mode
+ * @str: string to print
+ *
+ * Return: void
+ */
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (*(str + i) = '\0')
+	{
+		printf("%c", *(str + i));
+		i = i + 2;
+	}
+}
