620d410e921fc30ff8785465523088feb22f912f
beaconapollo <down4trust@gmail.com>
2022-04-19 20:16:46 +0300

    reverse string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -9,17 +9,16 @@
 
 void rev_string(char *s)
 {
-	int i = 0, len = 0, j = 0;
+	int i = 0, j = 0;
 	char *p;
 
-	p = s;
-	while (*p != '\0')
+	while (*(s + i) != '\0')
 	{
-		len++;
-		p++;
+		*(p + i) = *(s + i);
+		i++;
 	}
-	i = len - 1;
-	while (i >= 0 && j < len)
+	i -= 1;
+	while (i >= 0)
 	{
 		*(s + i) = *(p + j);
 		i--;
