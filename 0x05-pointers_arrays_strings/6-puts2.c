2d531baf9d56217c5679d71349895f9251b804eb
beaconapollo <down4trust@gmail.com>
2022-04-19 21:50:36 +0300

    Print character

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -8,12 +8,12 @@
  */
 void puts2(char *str)
 {
-        int i = 0;
+	int i = 0;
 
-        while (*(str + i) != '\0')
-        {
-                printf("%c", *(str + i));
-                i = i + 2;
+	while (*(str + i) != '\0')
+	{
+		printf("%c", *(str + i));
+		i = i + 2;
         }
-        printf("\n");
+	printf("\n");
 }
