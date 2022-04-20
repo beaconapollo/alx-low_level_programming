f0d66338a2db965421d0847c59f29ea4ef0a7373
beaconapollo <down4trust@gmail.com>
2022-04-20 06:44:43 +0300

    Print character

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,7 +1,7 @@
 #include "main.h"
-#include <stdio.h>
+
 /**
- * puts2 - entry mode
+ * puts2 - prints every other character of a  string to stdout
  * @str: string to print
  *
  * Return: void
@@ -12,8 +12,8 @@ void puts2(char *str)
 
 	while (*(str + i))
 	{
-		printf("%c", *(str + i));
+		_putchar(*(str + i));
 		i = i + 2;
 	}
-	printf("\n");
+	_putchar('\n');
 }
