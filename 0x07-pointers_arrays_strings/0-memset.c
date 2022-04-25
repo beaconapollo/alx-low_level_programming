10f6de623a552516485d9f8f3ed8384e551070d9
beaconapollo <down4trust@gmail.com>
2022-04-25 12:58:08 +0300

    Write memory

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,10 +1,11 @@
 #include "main.h"
 /*
- *_memset - function fills n bytes
+ *_memset - function fills n bytes with a constant byte
  */
-char *_memtest(char *s, char b, unsigned int n)
+char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i = 0;
+
 	while (i < n)
 	{
 		*(s + i) = b;
