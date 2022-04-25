2e3d6a9992937b78288d1cf3ec495db1601f83cd
beaconapollo <down4trust@gmail.com>
2022-04-25 09:30:29 +0300

    Password generator

--- 0x05-pointers_arrays_strings/comment101-keygen.c
+++ 0x05-pointers_arrays_strings/comment101-keygen.c
@@ -10,16 +10,36 @@
  */
 int main(void)
 {
+	// Initialize counter
 	int i = 0;
+
 	int randomizer = 0;
+
+	// Seed the random-number generator
+	// with current time so that the
+	// numbers will be different every time
+	srand(time(NULL));
+
+	// Array of numbers
 	char numbers[] = "0123456789";
+
+	// Array of all the special symbols
 	char symbols[] = "!@#$^&*?";
+
+	// Array of small alphabets
 	char letter[] = "abcdefghijklmnoqprstuvwyzx";
+
+	// Array of capital alphabets
 	char LETTER[] = "ABCDEFGHIJKLMNOQPRSTUYWVZX";
+
+	// Stores the random password
 	char password[N];
 
-	srand(time(NULL));
+	// To select the randomizer
+	// inside the loop
 	randomizer = rand() % 4;
+
+	// Iterate over the range [0, N]
 	for (i = 0; i < N; i++)
 	{
 		if (randomizer == 1)
