ad62991d11503d3e91df0bd56b861ea3145e7ce4
beaconapollo <down4trust@gmail.com>
2022-04-27 14:25:37 +0300

    print string with recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,9 +1,8 @@
 #include "main.h"
 #include <stdio.h>
-
-/*
+/**
  * _puts_recursion - prints a string followed by a new line
- * @s: a string to be printed
+ * @s: string to print
  *
  * Return: void
  */
