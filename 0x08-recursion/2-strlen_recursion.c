b7a5e79c9a682b7b9b5ed16c2664540b12b4a14d
beaconapollo <down4trust@gmail.com>
2022-04-27 15:31:11 +0300

    Print string length

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strlen_recursion - prints a string followed by a new line
+ * @s: string to print
+ *
+ * Return: void
+ */
+void _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return;
+	}
+	else
+	{
+		return (1 + _strlen_recursion(s + 1))
+	}
+}
