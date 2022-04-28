bd8e4a55d776e4418e03f7a30cbc2d0798b834bc
beaconapollo <down4trust@gmail.com>
2022-04-28 11:26:56 +0300

    Palindrome check

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,42 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * isPalindrome - checks if a string palindrome
+ * @inputString: a string to be checked
+ * @leftIndex: first index of a string
+ * @rightIndex: last index of a string
+ *
+ * Return: 1 if a string is a palindrome
+ */
+int isPalindrome(char *inputString, int leftIndex, int rightIndex)
+{
+	if (*inputString == '\0' || leftIndex < 0 || rightIndex < 0)
+		return (0);
+	if (leftIndex >= rightIndex)
+		return 1;
+	if (inputString[leftIndex] == inputString[rightIndex])
+	{
+		return (isPalindrome(inputString, leftIndex + 1, rightIdex - 1));
+	}
+	return (0);
+}
+/**
+ * is_palindrome - prints 1 a palindrome string 
+ * @s: a string to be checked
+ *
+ * Return: 1 if palindrome
+ */
+int is_palindrome(char *s)
+{
+	int lIndex = 0;
+	int rIndex = 0;
+	char *p;
+	p = s;
+
+	while (*p)
+	{
+		rIndex++;
+		p++;
+	}
+	return (isPalindrome(s, lIndex, rIndex));
+}
