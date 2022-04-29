7c4477be19d069bd6e876b7dd74e905762166688
beaconapollo <down4trust@gmail.com>
2022-04-29 21:19:07 +0300

    Prints its name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _puts_recursion - prints a string followed by a new line
+ * @s: string to print
+ *
+ * Return: void
+ */
+int main(int argc, char *argv)
+{
+	printf("%s", argv);
+	return(0);
+}
