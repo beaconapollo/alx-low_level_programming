c30f26a1543d8090a2338ee0c85a8d53a17b4428
beaconapollo <down4trust@gmail.com>
2022-04-29 21:58:59 +0300

    Prints its name

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -9,6 +9,16 @@
 
 int main(int argc, char *argv[])
 {
-	printf("%d\n", argc - 1);
+	int i;
+
+	if (argc > 1)
+	{
+		for (i = 0; i < argc; i++);
+		printf("%d\n", i - 1)
+	}
+	else
+	{
+		printf("%d\n", argc - 1);
+	}
 	return (0);
 }
