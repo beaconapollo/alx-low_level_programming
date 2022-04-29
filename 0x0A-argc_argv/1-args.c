1003b5a2f61ed9afc9da31047921d5fab4cdacb9
beaconapollo <down4trust@gmail.com>
2022-04-29 21:46:01 +0300

    Prints its name

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the name of the program
+ * @argc: Number of command line arguments
+ * @argv: Array name
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
