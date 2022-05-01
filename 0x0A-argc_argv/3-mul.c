983ee8eda43fff45388ace6bcc72f23d2b0e9b9e
beaconapollo <down4trust@gmail.com>
2022-05-01 17:09:55 +0300

    Multiplcation of arguments

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,4 +1,3 @@
-#include "main.h"
 #include <stdlib.h>
 #include <stdio.h>
 /**
@@ -12,16 +11,16 @@ int main(int argc, char *argv[])
 {
 	int a, b;
 
-	if(argc < 3 || argc > 3)
+	if(argc == 3)
 	{
-		printf("Error\n");
-                return (1);
+		a = atoi(argv[1]);
+		b = atoi(argv[2]);
+		printf("%d\n", a * b);
 	}
 	else
 	{
-                a = atoi(argv[1]);
-                b = atoi(argv[2]);
-                printf("%d\n", a * b);
-        }
+		printf("Error\n");
+		return (1);
+	}
 	return (0);
 }
