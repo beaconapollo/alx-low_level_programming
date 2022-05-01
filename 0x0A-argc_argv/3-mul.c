11dcaa6a3df486ad71f6465c06a6009cdb55b647
beaconapollo <down4trust@gmail.com>
2022-05-01 16:57:25 +0300

    Multiplcation of arguments

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,6 +1,5 @@
 #include "main.h"
 #include <stdio.h>
-
 /**
  * main - prints multiplication of arguments passed into main
  * @argc: Number of command line arguments
@@ -8,23 +7,20 @@
  *
  * Return: 1 if arguments <> 3 else 0
  */
-
 int main(int argc, char *argv[])
 {
-	int a, b, m;
+	int a, b;
 
 	if(argc == 3)
 	{
-		a = _atoi(*(argv + 1));
-		b = _atoi(*(argv + 2));
-		m = a * b;
-		printf("%i\n", m);
+		a = _atoi(argv[1]);
+		b = _atoi(argv[2]);
+		printf("%d\n", a * b);
 	}
 	else
 	{
 		printf("Error\n");
 		return (1);
 	}
-
 	return (0);
 }
