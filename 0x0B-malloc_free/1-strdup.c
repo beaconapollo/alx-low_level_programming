a57abef36355e4cd05b99e1603400a14434098a9
beaconapollo <down4trust@gmail.com>
2022-05-04 02:40:14 +0300

    copy string with size by malloc

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -11,18 +11,23 @@ char *_strdup(char *str)
 	int i = 0, size;
 	char *s;
 
-	while (*(str + i))
+	if (str == NULL)
 	{
-		i++;
-	}
-	size = i + 1;
-	s = (char *)malloc(size * sizeof(char));
-	if (s == NULL)
 		return (NULL);
-	for (i = 0; i < size; i++)
-	{
-		s[i] = str[i];
 	}
-	s[i + 1] = '\0';
+	else
+	{
+		while (*(str + i))
+		{
+			i++;
+		}
+		size = i + 1;
+		s = (char *)malloc(size * sizeof(char));
+		for (i = 0; i < size; i++)
+		{
+			s[i] = str[i];
+		}
+		s[i + 1] = '\0';
+	}
 	return (s);
 }
