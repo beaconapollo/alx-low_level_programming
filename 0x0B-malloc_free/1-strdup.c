4e6c1510270cfd332f245d0cc040ebd94890df66
beaconapollo <down4trust@gmail.com>
2022-05-04 02:30:21 +0300

    copy string with size by malloc

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * strdup - returns a pointer to a new string which is a duplicate of string str
+ * @str: input string
+ * Return: pointer of thestring
+ */
+char *_strdup(char *str)
+{
+	int i = 0, size;
+	char *s;
+
+	while (*(str + i))
+	{
+		i++;
+	}
+	if (i == 0)
+	{
+		s = NULL;
+	}
+	else
+	{
+		size = i + 1;
+		s = (char *)malloc(size * sizeof(char));
+		if (s == NULL)
+			return (NULL);
+		for (i = 0; i < size; i++)
+		{
+			s[i] = str[i];
+		}
+	}
+	return (s);
+}
