91fd1f86eb2b67d8090981e67c90224f15d4a0bc
beaconapollo <down4trust@gmail.com>
2022-05-12 14:08:04 +0300

    Search a number with pointer array

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,29 +1,28 @@
 #include <stdio.h>
 #include "function_pointers.h"
 /**
- * int_index - searches an integer
- * @array: array to search on
+ * int_index - function that searches integer
+ * @array: array with parameters
  * @size: the number of elements in the array
  * @cmp: pointer to the function to be used to compare values
+ * Return: the index of an element
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (array == NULL || cmp == NULL)
-	{
-		return (-1);
-	}
-
 	if (size <= 0)
 	{
 		return (-1);
 	}
-	for (i = 0; i < size; i++)
+	if (array != NULL && cmp != NULL)
 	{
-		if (cmp(array[i]))
+		for (i = 0; i < size; i++)
 		{
-			return (i);
+			if (cmp(array[i]))
+			{
+				return (i);
+			}
 		}
 	}
 	return (-1);
