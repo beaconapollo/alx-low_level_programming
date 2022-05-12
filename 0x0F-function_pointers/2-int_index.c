ab89fe52e416b5fc89d1ae8ac45a55888116b7ce
beaconapollo <down4trust@gmail.com>
2022-05-12 14:03:00 +0300

    Search a number with pointer array

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -14,19 +14,17 @@ int int_index(int *array, int size, int (*cmp)(int))
 	{
 		return (-1);
 	}
+
 	if (size <= 0)
 	{
 		return (-1);
 	}
-	else
+	for (i = 0; i < size; i++)
 	{
-		for (i = 0; i < size; i++)
+		if (cmp(array[i]))
 		{
-			if (cmp(array[i]))
-			{
-				return (i);
-			}
+			return (i);
 		}
-		return (-1);
 	}
+	return (-1);
 }
