6477f8b1f4dee12ac24643eacce83bf3c9c0d10c
mercy83 <down4trust@gmail.com>
2022-05-24 12:51:46 -0700

    Singly linked list

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_listint: prints all the elements of a listint_t list
+ * h: linked list to be printed
+ * Return: the number of nodes
+ *
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	listint_t *
+}
