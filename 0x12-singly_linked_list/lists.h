31673debcd65576c7ccd3f8ceb79195f1686dfb6
beaconapollo <down4trust@gmail.com>
2022-05-25 14:53:23 +0300

    Print elements of Linked list

--- 0x12-singly_linked_list/lists.h
+++ 0x12-singly_linked_list/lists.h
@@ -8,14 +8,16 @@
  * @next: points to the next node
  *
  * Description: singly linked list node structure
+ * for Holberton project
  */
+
 typedef struct list_s
 {
-    char *str;
-    unsigned int len;
-    struct list_s *next;
-}list_t;
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
 
-size_t print_list(const list_t *);
+size_t print_list(const list_t *h);
 
 #endif
