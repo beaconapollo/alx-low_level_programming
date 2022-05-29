bef160e523755ef1098b5dda656469384e0ec78d
mercy83 <down4trust@gmail.com>
2022-05-29 04:49:28 -0700

    Convert decimal to binary

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,19 +1,25 @@
 #include <stdio.h>
 #include "main.h"
 /**
+ * _start_from_MSB - recursion function to convert MSB first
+ * @d: a decimal number
+ */
+void _start_from_MSB(unsigned long int d)
+{
+	if (d == 0)
+		return;
+	_start_from_MSB(d >> 1);
+	printf("%c", (d & 1) == 0 ? '0' : '1');
+}
+/**
  * print_binary - converts a number to binary
  * @n: a decimal number to be converted
  * Return: the binary number otherwise 0
  */
 void print_binary(unsigned long int n)
 {
-	char *b = " ";
-	int i = 0;
-	while (n > 0)
-	{
-		*(b + i) = ((n && 1) == 0 ? '0' : '1');
-		n >>= 1;
-		i++;
-	}
-	printf("%d", i);
+	if (n == 0)
+		printf("%c", '0');
+	else
+		_start_from_MSB(n);
 }
