e88bbf0c9cf8924dbde294b0dc429aa5aafb94a3
beaconapollo <down4trust@gmail.com>
2022-05-29 20:37:12 +0300

    Change a bit

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -2,9 +2,9 @@
 
 /**
  * set_bit - sets the value of a bit to 1 at a given index
- * @n: the number to set
- * @index: the index in the number
- * Return: 1 if it worked or -1 if an error occured
+ * @n: type unsigned long int input pointer
+ * @index: type unsigned int position of unit
+ * Return: 1 if works -1 if not
  */
 
 int set_bit(unsigned long int *n, unsigned int index)
