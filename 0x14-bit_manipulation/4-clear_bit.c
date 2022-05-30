a53c6b1bf12065c4c096a9bdc34bc1c0458d2fe2
beaconapollo <101219024+beaconapollo@users.noreply.github.com>
2022-05-30 09:54:15 +0300

    Set bit to 0

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * clear_bit - sets the value of a bit to 0 at a given index
+ * @n: type unsigned long int input pointer
+ * @index: type unsigned int position of unit
+ * Return: 1 if works -1 if not
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	if (index > 63)
+		return (-1);
+	else
+	{
+		*n &= ~(1 << index);
+		return (1);
+	}
+}
