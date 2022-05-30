a830c92b5dc1a700b7a1c3a883e40d61c6054946
beaconapollo <101219024+beaconapollo@users.noreply.github.com>
2022-05-30 10:03:08 +0300

    Change a single bit to 0

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -3,7 +3,7 @@
 /**
  * clear_bit - sets the value of a bit to 0 at a given index
  * @n: type unsigned long int input pointer
- * @index: type unsigned int position of unit
+ * @index: index of n to be changed to 0
  *
  * Return: 1 if works -1 if not
  */
