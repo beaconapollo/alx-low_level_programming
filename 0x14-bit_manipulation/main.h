78c45a836e58c9a60c0725a7fb5ef96a8dcfdb54
beaconapollo <101219024+beaconapollo@users.noreply.github.com>
2022-05-30 09:56:12 +0300

    Update main.h

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -5,3 +5,4 @@ unsigned int binary_to_uint(const char *);
 void print_binary(unsigned long int);
 int get_bit(unsigned long int, unsigned int);
 int set_bit(unsigned long int *, unsigned int);
+int clear_bit(unsigned long int *, unsigned int);
