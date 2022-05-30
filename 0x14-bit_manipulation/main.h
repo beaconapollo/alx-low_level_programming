cb132cfbd48a34f2ce253007ed572f157cf42961
beaconapollo <101219024+beaconapollo@users.noreply.github.com>
2022-05-30 09:59:15 +0300

    Update main.h

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,3 +1,6 @@
+#ifndef MAIN_H
+#define MAIN_H
+
 #include <stdio.h>
 
 int _putchar(char);
@@ -6,3 +9,5 @@ void print_binary(unsigned long int);
 int get_bit(unsigned long int, unsigned int);
 int set_bit(unsigned long int *, unsigned int);
 int clear_bit(unsigned long int *, unsigned int);
+
+#endif
