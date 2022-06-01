07c00e3399e6a1743e631ef4e9c23191f658397d
beaconapollo <101219024+beaconapollo@users.noreply.github.com>
2022-06-01 16:00:16 +0300

    Update 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -9,24 +9,29 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	ssize_t file, ch, out;
-	char *buffer;
+	ssize_t file, let, w;
+	char *text;
+
+	text = malloc(letters);
+	if (text == NULL)
+		return (0);
 
 	if (filename == NULL)
 		return (0);
+
 	file = open(filename, O_RDONLY);
+
 	if (file == -1)
+	{
+		free(text);
 		return (0);
-	buffer = malloc(letters);
-	if (buffer == NULL)
-		return (0);
-	ch = read(file, buffer, letters);
-	if (ch == -1)
-		return (0);
-	out = write(STDIN_FILENO, buffer, ch);
-	if (out == -1)
-		return (0);
+	}
+
+	let = read(file, text, letters);
+
+	w = write(STDOUT_FILENO, text, let);
+
 	close(file);
-	free(buffer);
-	return (out);
+
+	return (w);
 }
