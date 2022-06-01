8546d183ba12aea42e93f6f5d6121bd2524f7b73
beaconapollo <101219024+beaconapollo@users.noreply.github.com>
2022-06-01 22:17:11 +0300

    Update 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -8,11 +8,11 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-  int in, out, i = 0;
+	int in, out, i = 0;
 
 	if (filename == NULL)
-	  return (-1);
-  if (text_content == NULL)
+		return (-1);
+	if (text_content == NULL)
 		return (1);
   
 	in = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0664);
