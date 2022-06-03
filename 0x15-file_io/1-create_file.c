7620ca0175f405f2b2f2dec3915b30170787f49f
beaconapollo <101219024+beaconapollo@users.noreply.github.com>
2022-06-03 13:00:48 +0300

    Update 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,7 +12,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	file_des = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	file_des = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (file_des == -1)
 		return (-1);
 	if (text_content == NULL)
