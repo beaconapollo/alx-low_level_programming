e5a4d368d1b9fcc3403741f5e1d8c15dbc273e91
beaconapollo <101219024+beaconapollo@users.noreply.github.com>
2022-06-03 15:39:48 +0300

    copy file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "main.h"
+
 /**
-  * main - entry point
-  * @argc: number of arguments
-  * @argv: array of argument tokens
-  * Return: 0 on success
-  */
+* main - entry point
+* @argc: number of arguments
+* @argv: array of argument tokens
+* Return: 0
+*/
+
 int main(int argc, char *argv[])
 {
 	int file_from, file_to, rd_status, wr_status;
@@ -21,26 +23,31 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, perm);
+	file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, perm);
 	if (file_to == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		close(file_from), exit(99);
 	}
-
 	rd_status = 1;
 	while (rd_status)
 	{
 		rd_status = read(file_from, buffer, 1024);
 		if (rd_status == -1)
-			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]), exit(98);
+		{
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+			exit(98);
+		}
 		wr_status = write(file_to, buffer, rd_status);
 		if (wr_status < rd_status)
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
 	}
+
 	if (close(file_from) == -1)
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from), exit(100);
+
 	if (close(file_to) == -1)
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to), exit(100);
+
 	return (0);
 }
