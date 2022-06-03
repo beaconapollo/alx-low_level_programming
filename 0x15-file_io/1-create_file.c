2a2b2edebb9866d0b7d1fd8164d68c40e73894fa
beaconapollo <101219024+beaconapollo@users.noreply.github.com>
2022-06-03 11:57:15 +0300

    Create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,30 +1,32 @@
 #include "main.h"
 
 /**
- * create_file - creates a file.
- * @filename: the name of the file to create.
- * @text_content: a NULL terminated string to write to the file
- * Return: 1 on success, -1 on failure.
+ * create_file - a function that creates a file
+ * @filename: the name of the file to create
+ * @text_content: contentss of the file
+ * Return: 1 on success, -1 on failure
  */
 int create_file(const char *filename, char *text_content)
 {
-	int in, out, i = 0;
+	int file_des, wr, i = 0;
 
 	if (filename == NULL)
 		return (-1);
-	if (text_content == NULL)
-		return (1);
-  
-	in = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0664);
-	if (in == -1)
+	file_des = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (file_des == -1)
 		return (-1);
-	
+	if (text_content == NULL)
+	{
+		close(file_des);
+		return (1);
+	}
 	while (text_content[i])
 		i++;
-	out = write(in, text_content, i);
-	if (out == -1)
+	wr = write (file_des, text_content, i);
+	if (wr == -1)
 		return (-1);
-  
-	close(in);
+	if (wr == -1)
+		return (-1);
+	close(file_des);
 	return (1);
 }
