fbc48bb61388e610d983c86edd677f05c3eb368f
beaconapollo <101219024+beaconapollo@users.noreply.github.com>
2022-06-03 13:07:35 +0300

    Update 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -25,8 +25,6 @@ int create_file(const char *filename, char *text_content)
 	wr = write (file_des, text_content, i);
 	if (wr == -1)
 		return (-1);
-	if (wr == -1)
-		return (-1);
 	close(file_des);
 	return (1);
 }
