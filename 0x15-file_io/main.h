767694bd7fbd2e2de4c81fdbf38238e0085f1f2a
beaconapollo <101219024+beaconapollo@users.noreply.github.com>
2022-06-03 13:39:16 +0300

    Update Main.h

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -9,6 +9,7 @@
 #include <stdlib.h>
 
 ssize_t read_textfile(const char *, size_t);
-int create_file(const char *filename, char *text_content);
+int create_file(const char *, char *);
+int append_text_to_file(const char *, char *);
 
 #endif
