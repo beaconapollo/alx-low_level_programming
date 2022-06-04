c8341daea0aac1230a5b3c5005301cde81ac3b6b
beaconapollo <101219024+beaconapollo@users.noreply.github.com>
2022-06-04 10:28:10 +0300

    Allocate memory

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,1 +1,15 @@
+#include "main.h"
 
+/**
+ * malloc_checked - allocates memory using malloc
+ * @b: the allocated memory
+ */
+void *malloc_checked(unsigned int b)
+{
+        void *s = malloc(b);
+
+        if (s)
+                return (s);
+        free(s);
+        exit(98);
+}
