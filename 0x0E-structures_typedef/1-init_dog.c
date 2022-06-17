08bbb070023542e1fa8d5eb1c2a80953d9fa47c5
beaconapollo <101219024+beaconapollo@users.noreply.github.com>
2022-06-18 00:03:43 +0300

    init struct

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,14 @@
+#include "dog.h"
+/**
+ * init_dog - initializing dog
+ * @d: passing dog structure
+ * @name: name of a dog
+ * @age: age of a dog
+ * @owner: owner of a dog
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
