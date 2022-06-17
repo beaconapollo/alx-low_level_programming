05a849e0ca749ed8c27752b4fda01074655e5f92
beaconapollo <101219024+beaconapollo@users.noreply.github.com>
2022-06-18 00:34:04 +0300

    Task 2-Print dog struct

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -11,11 +11,10 @@ void print_dog(struct dog *d)
 			printf("Name: %s\n", d->name);
 		else
 			printf("Name: (nil)\n");
-		printf("Age: %f\n", d->age);
+		printf("Age: %.6f\n", d->age);
 		if (d->owner != NULL)
 			printf("Owner: %s\n", d->owner);
 		else
-			printf("Owner: (nil)");
-
+			printf("Owner: (nil)\n");
 	}
 }
