eb13e7e07f2ae417c42e40cd99dfab96cb9da165
beaconapollo <101219024+beaconapollo@users.noreply.github.com>
2022-06-18 12:40:17 +0300

    Add files via upload

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,23 +1,47 @@
 #include <stdio.h>
 /**
- * main - entry point
- * Return: Always 0
+ * main - prints the first 98 Fibonacci
+ * Return: 0 Value if it works
  */
 int main(void)
 {
-	int i = 0;
-	long int x = 1, y = 2, z;
+	unsigned long int w = 0, x = 0, y = 1, z;
+	unsigned long int x1, x2, y1, y2, z1, z2;
 
-	while (i < 98)
+	while (w < 92)
 	{
-		if (i < 97)
-			printf("%lu, ", x);
-		else
-			printf("%lu", x);
 		z = x + y;
 		x = y;
 		y = z;
-		i++;
+		printf("%lu, ", z);
+		w++;
+	}
+		x1 = x / 10000000000;
+		x2 = x % 10000000000;
+		y1 = y / 10000000000;
+		y2 = y % 10000000000;
+	while (w < 98)
+	{
+		z1 = x1 + y1;
+		z2 = x2 + y2;
+		if (z2 > 10000000000)
+		{
+			z1 = z1 + 1;
+			z2 = z2 % 10000000000;
+		}
+		if (w < 97)
+		{
+			printf("%lu%lu, ", z1, z2);
+			x1 = y1;
+			x2 = y2;
+			y1 = z1;
+			y2 = z2;
+		}
+		else
+		{
+			printf("%lu%lu\n", z1, z2);
+		}
+		w++;
 	}
 	return (0);
 }
