cfed1202da49dbe1c79962220e6ae2b35b726a3e
beaconapollo <101219024+beaconapollo@users.noreply.github.com>
2022-06-18 22:26:32 +0300

    Update 100-argstostr.c

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -12,5 +12,28 @@ char *argstostr(int ac, char **av)
 {
 	int i, j, strLen, k;
 	char *newStr;
+	
+	k = 0;
+	strLen = 0;
+	if (ac == 0 || av == NULL)
+		return (NULL);
+	
+	for (i = 0; i < ac; i++)
+		for (j = 0; *(*(av + i) + j); j++)
+			strLen++;
+	newStr = malloc((strLen + ac + l) * sizeof(char));
+	if (newStr == NULL)
+		return (NULL);
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; *(*(av + i) + j) != '\0'; j++)
+		{
+			*(newStr + k) = *(*(av + i) + j);
+			k++;
+		}
+		*(newStr + k) = '\n';
+		k++;
+	}
+        *(newStr + k) = '\0';
 	return (newStr);
 }
