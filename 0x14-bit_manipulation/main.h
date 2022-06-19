deadbd4f884b7a2be681024f4180c65248c7798d
beaconapollo <101219024+beaconapollo@users.noreply.github.com>
2022-06-19 22:32:34 +0300

    Add files via upload

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,14 +1,15 @@
-#ifndef MAIN_H
-#define MAIN_H
-
+#ifndef BIT_MANIPULATION
+#define BIT_MANIPULATION
+#include <stdlib.h>
+#include <string.h>
 #include <stdio.h>
 
-int _putchar(char);
-unsigned int binary_to_uint(const char *);
-void print_binary(unsigned long int);
-int get_bit(unsigned long int, unsigned int);
-int set_bit(unsigned long int *, unsigned int);
-int clear_bit(unsigned long int *, unsigned int);
-unsigned int flip_bits(unsigned long int, unsigned long int);
-
+int _putchar(char c);
+unsigned int binary_to_uint(const char *b);
+void print_binary(unsigned long int n);
+int get_bit(unsigned long int n, unsigned int index);
+int set_bit(unsigned long int *n, unsigned int index);
+int clear_bit(unsigned long int *n, unsigned int index);
+unsigned int flip_bits(unsigned long int n, unsigned long int m);
+int get_endianness(void);
 #endif
