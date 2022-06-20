7889ad41670cda0409793ab53d66693d15a89956
beaconapollo <101219024+beaconapollo@users.noreply.github.com>
2022-06-20 10:54:00 +0300

    Update 2-add_node.c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -6,34 +6,30 @@
  * add_node - adds a new node at the beginning of a list_t list
  * @head: pointer to the first node
  * @str: char pointer for data to be added
- * Return: the address of the new element, otherwise NULL
- *
+ * Return: the address of the new element, otherwise NULL *
  */
-
 list_t *add_node(list_t **head, const char *str)
 {
-	int i = 0;
-	list_t *new_head;
+	list_t *new_node;
+	int c;
 
-	new_head = malloc(sizeof(list_t));
-
-	if (new_head == NULL)
+	for (c = 0; dup_str[c] != '\0'; c++)
+		;
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
 	{
-		free(new_head);
+		free(new_node);
 		return (NULL);
 	}
-	new_head->next = *head;
-	*head = new_head;
-	new_head->str = strdup(str);
-	if (new_head->str == NULL)
+	new_node->str = strdup(str);
+	if (new_node->str == NULL)
 	{
-		free(new_head);
-		return(NULL);
+		free(new_node);
+		return (NULL);
 	}
-	while (str[i])
-	{
-		i++;
-	}
-	new_head->len = i;
-	return (new_head);
+	new_node->len = c;
+	new_node->next = *head;
+
+	*head = new_node;
+	return (*head);
 }
