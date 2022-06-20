967e2c3bb6f3e460d32fbfb5c4abf39cbc800ff4
beaconapollo <101219024+beaconapollo@users.noreply.github.com>
2022-06-20 10:57:36 +0300

    Add files via upload

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -6,31 +6,33 @@
  * add_node - adds a new node at the beginning of a list_t list
  * @head: pointer to the first node
  * @str: char pointer for data to be added
- * Return: the address of the new element, otherwise NULL *
+ * Return: the address of the new element, otherwise NULL
+ *
  */
+
 list_t *add_node(list_t **head, const char *str)
 {
-        int i = 0;
-        list_t *new_head;
+	int i = 0;
+	list_t *new_head;
 
-        new_head = malloc(sizeof(list_t));
+	new_head = malloc(sizeof(list_t *));
 
-        if (new_head == NULL)
-        {
-                return (NULL);
-        }
-        new_head->next = *head;
-        *head = new_head;
-        new_head->str = strdup(str);
-        if (!new_head->str)
-        {
-                free(new_head);
-                return(NULL);
-        }
-        while (str[i])
-        {
-                i++;
-        }
-        new_head->len = i;
-        return (new_head);
+	if (new_head == NULL)
+	{
+		return (NULL);
+	}
+	new_head->next = *head;
+	*head = new_head;
+	new_head->str = strdup(str);
+	if (!new_head->str)
+	{
+		free(new_head);
+		return(NULL);
+	}
+	while (str[i])
+	{
+		i++;
+	}
+	new_head->len = i;
+	return (new_head);
 }
