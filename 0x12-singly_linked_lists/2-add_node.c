5199d971d8918a732d57fdea7130aa8f28bcd507
beaconapollo <101219024+beaconapollo@users.noreply.github.com>
2022-06-20 10:52:43 +0300

    Update 2-add_node.c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -37,29 +37,3 @@ list_t *add_node(list_t **head, const char *str)
 	new_head->len = i;
 	return (new_head);
 }
-
-list_t *add_node(list_t **head, const char *str)
-{
-	list_t *new_node;
-	int c;
-
-	for (c = 0; dup_str[c] != '\0'; c++)
-		;
-	new_node = malloc(sizeof(list_t));
-	if (new_node == NULL)
-	{
-		free(new_node);
-		return (NULL);
-	}
-	new_node->str = strdup(str);
-	if (new_node->str == NULL)
-	{
-		free(new_node);
-		return (NULL);
-	}
-	new_node->len = c;
-	new_node->next = *head;
-
-	*head = new_node;
-	return (*head);
-}
