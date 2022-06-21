92a8879cd10b142c7bfce4e000c761ed5b5a9f72
beaconapollo <101219024+beaconapollo@users.noreply.github.com>
2022-06-21 10:02:25 +0300

    Create a new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,44 +1,42 @@
 #include "dog.h"
 /**
- * new_dog - creates a new dog
- * @name: name of a new dog
- * @age: age of a new dog
- * @owner: owner of a new dog
+ * new_dog - creats a new dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *dog1;
-	int i = 0;
-	int nlen = 0, olen = 0;
+	dog_t *d;
+	int ln = 0, lo = 0, i;
 
-	if (name == NULL || owner == NULL)
+	if (name == NULL || age < 0 || owner == NULL)
 		return (NULL);
-	dog1 = malloc(sizeof(dog_t));
-	if (dog1 == NULL)
+
+	d = malloc(sizeof(dog_t));
+	if (d == NULL)
 		return (NULL);
-	while (*(name + nlen))
-		nlen++;
-	nlen += 1;
-	dog1->name = malloc(nlen * sizeof(char));
-	if (dog1->name == NULL)
+
+	while (*(name + ln))
+		ln++;
+	d->name = malloc(sizeof(char) * (ln + 1));
+	if (d->name == NULL)
 	{
-		free(dog1);
+		free(d);
 		return (NULL);
 	}
-	for (i = 0; i < nlen; i++)
-		dog1->name[i] = name[i];
-	dog1->age = age;
-	while (*(owner + olen))
-		olen++;
-	olen += 1;
-	dog1->owner = malloc(olen * sizeof(char));
-	if (dog1->owner == NULL)
-	{
-		free(dog1->name);
-		free(dog1);
+	for (i = 0; *(name + i); i++)
+		d->name[i] = name[i];
+
+	d->age = age;
+
+	while (*(owner + lo))
+		lo++;
+	d->owner = malloc(sizeof(char) * (lo + 1));
+	if (d->owner == NULL)
 		return (NULL);
-	}
-	for (i = 0; i < olen; i++)
-		dog1->owner[i] = owner[i];
-	return (dog1);
+	for (i = 0; *(owner + i); i++)
+		d->owner[i] = owner[i];
+
+	return (d);
 }
