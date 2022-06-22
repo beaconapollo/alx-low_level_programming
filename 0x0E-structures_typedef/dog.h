2939ca341d2d871b7a48b9ac36913891fd6d890d
beaconapollo <101219024+beaconapollo@users.noreply.github.com>
2022-06-22 15:04:18 +0300

    Add files via upload

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -22,4 +22,6 @@ typedef struct dog dog_t;
 void init_dog(struct dog *d, char *, float, char *);
 void print_dog(struct dog *);
 dog_t *new_dog(char *, float, char *);
+void free_dog(dog_t *);
+
 #endif
