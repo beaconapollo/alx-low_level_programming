62043366c821f6f7cdda9313fbcddf2e65221187
beaconapollo <101219024+beaconapollo@users.noreply.github.com>
2022-06-24 12:14:48 +0300

    Add node at the end

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -10,44 +10,49 @@
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *current;
-	list_t *new_node;
+	list_t *current, *end_node;
 	char *dupstr;
 	int i = 0, len;
 
-	if (str == NULL)
+	end_node = malloc(sizeof(list_t));
+	if (end_node == NULL)
 		return (NULL);
-	while (*(str + i))
-		i++;
-	len = i + 1;
-	i = 0;
 
-	dupstr = malloc(sizeof(char) * len);
-	if (dupstr == NULL)
-		return (NULL);
-	while (*(str + i))
+	if (str)
 	{
-		dupstr[i] = str[i];
-		i++;
+		while (*(str + i))
+			i++;
+		len = i + 1;
+		i = 0;
+
+		dupstr = malloc(sizeof(char) * len);
+		if (dupstr == NULL)
+			return (NULL);
+		while (*(str + i))
+		{
+			dupstr[i] = str[i];
+			i++;
+		}
+		dupstr[i] = '\0';
+
+		end_node->str = dupstr;
+		end_node->len = len - 1;
 	}
-	dupstr[i] = '\0';
-
-	new_node = malloc(sizeof(list_t));
-	if (new_node == NULL)
-		return (NULL);
-
-	new_node->str = dupstr;
-	new_node->len = len - 1;
-	new_node->next = NULL;
-
-	if (*head == NULL)
+	else
 	{
-		*head = new_node;
-		return (*head);
+		end_node->str = NULL;
+		end_node->len = 0;
 	}
-	current = *head;
-	while (current->next != NULL)
-		current = current->next;
-	current->next = new_node;
-	return (*head);
+	end_node->next = NULL;
+
+	if (*head)
+	{
+		current = *head;
+		while (current->next != NULL)
+			current = current->next;
+		current->next = end_node;
+	}
+	else
+		*head = end_node;
+	return (end_node);
 }
